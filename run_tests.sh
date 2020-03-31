c5a4c5a841729769327fa84f4f3abac963fee0c4
Abe <abe149@gmail.com>
2020-03-31 08:38:08 -0400

    oops: my preceding check-in required "terry" to be _installed_ in order for "<repo>/run_tests.sh" to work

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ declare -i num_passed=0
 
 for testfile in `find tests/ -maxdepth 1 -type f`; do
   echo === $testfile ===
-  DEBUG=99 terry 'python([2-9](\.[[:digit:]])?)?' "$testfile"
+  DEBUG=99 ./bin/terry 'python([2-9](\.[[:digit:]])?)?' "$testfile"
   if [ 0 -eq $? ]; then
     num_passed+=1
   else
