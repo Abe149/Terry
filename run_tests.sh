881e3d124484c303727547d168355abcd9e52215
Abe <abe149@gmail.com>
2020-03-31 08:35:42 -0400

    added "/run_tests.sh", which already works [at least on MOSX 10.6.8]

---
+++ run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+declare -i num_failed=0
+declare -i num_passed=0
+
+for testfile in `find tests/ -maxdepth 1 -type f`; do
+  echo === $testfile ===
+  DEBUG=99 terry 'python([2-9](\.[[:digit:]])?)?' "$testfile"
+  if [ 0 -eq $? ]; then
+    num_passed+=1
+  else
+    num_failed+=1
+  fi
+  echo
+done
+
+echo
+
+if [ 0 -eq $num_failed ]; then
+  echo "All ($num_passed) tests passed."
+else
+  echo "$num_passed tests passed, $num_failed failed."
+fi
