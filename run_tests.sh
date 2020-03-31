b7df53d855b744d73c15a9d30c1b06afb60a594f
Abe <abe149@gmail.com>
2020-03-31 09:00:23 -0400

    "/run_tests.sh": fixed a minor bug [in the old loop], added a loop to test the shebang-enabled test cases via direct exection [but only when "terry" is installed]

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 declare -i num_failed=0
 declare -i num_passed=0
 
-for testfile in `find tests/ -maxdepth 1 -type f`; do
+for testfile in `find tests/ -maxdepth 1 -type f -name '*.py'`; do
   echo === $testfile ===
   DEBUG=99 ./bin/terry 'python([2-9](\.[[:digit:]])?)?' "$testfile"
   if [ 0 -eq $? ]; then
@@ -15,6 +15,19 @@ for testfile in `find tests/ -maxdepth 1 -type f`; do
 done
 
 echo
+
+if which terry >/dev/null 2>/dev/null; then
+  for testfile in `find tests/ -maxdepth 1 -type f -name '*.py' -perm +111`; do
+    echo === $testfile ===
+    DEBUG=99 "$testfile"
+    if [ 0 -eq $? ]; then
+      num_passed+=1
+    else
+      num_failed+=1
+    fi
+    echo
+  done
+fi
 
 if [ 0 -eq $num_failed ]; then
   echo "All ($num_passed) tests passed."
