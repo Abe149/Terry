1bd876bbe8e7c4f2162542e2197aa017e28e0331
Abe <abe149@gmail.com>
2020-04-28 13:46:41 -0400

    made both of the Py3 tests allow consideration of interpreters named e.g. "python3m", "python3mu", "python3.0m", and "python3.0mu" [even after the resolution of symlinks in non-VE mode if/when the potentially-dangerous feature that I have given Terry "himself" but have not yet committed yet -- to be committed soon in disabled form]

--- tests/py3_or_above_only___with_env-indirected_shebang.py
+++ tests/py3_or_above_only___with_env-indirected_shebang.py
@@ -1,2 +1,2 @@
-#!/usr/bin/env terry python([3-9](\.[[:digit:]]+)?)?
+#!/usr/bin/env terry python([3-9](\.[[:digit:]]+)?(m|mu)?)?
 print ("HI", end='\n')
--- tests/py3_or_above_only___with_terry-direct_shebang.py
+++ tests/py3_or_above_only___with_terry-direct_shebang.py
@@ -1,2 +1,2 @@
-#!/usr/local/bin/terry python([3-9](\.[[:digit:]]+)?)?
+#!/usr/local/bin/terry python([3-9](\.[[:digit:]]+)?(m|mu)?)?
 print ("HI", end='\n')
